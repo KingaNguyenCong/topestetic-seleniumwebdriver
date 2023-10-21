c227a715d63440fac40d23a4836f5cbd6c084d84
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-21 16:11:44 +0200

    3

--- Projekt1.py
+++ Projekt1.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+
 import time
 
 driver = webdriver.Chrome()
 driver.get('https://www.topestetic.pl/')
+
+time.sleep(5)
+search_button = driver.find_element(By.NAME,'q')
+driver.find_element(By.NAME,'q').submit()
+#search_button.click()
+#search_button.send_keys('Serum')
+
+time.sleep(10)
+
+
+
+
+
 
 driver.quit()
 
